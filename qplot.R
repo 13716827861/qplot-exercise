c47996dd04137c35b1bc60f94786f984241e824e
T&T Development <99367973+terrathedeveloper@users.noreply.github.com>
2022-07-20 07:00:05 -0500

    Create qplot.R

---
+++ qplot.R
@@ -0,0 +1,13 @@
+#QPlot Assignment
+
+# Create a scatterplot with values for x and y
+
+
+# Create a histogram using the roll dice function to show frequency of rolls
+
+
+# Create a histogram adding weights to the dice values by setting the prob argument within the sample method
+
+
+
+
